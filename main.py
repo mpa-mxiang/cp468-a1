3ab0ec86646b52ac7dcdb62c7174b02aa32a8a6e
Jiachun Xiang <jcxiang1230@gmail.com>
2020-10-07 20:20:35 -0400

    fixed error

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 
 
-# def main():
-puzzle_size = int(input(
-    "What size of puzzle you want to have? Enter 8, 15 or 24 Please: "))
-while(puzzle_size != 8 or puzzle_size != 15 or puzzle_size != 24):
+def main():
     puzzle_size = int(input(
         "What size of puzzle you want to have? Enter 8, 15 or 24 Please: "))
-print("You choose {}-puzzle, now it is  time to create the puzzle for you...".format(puzzle_size))
-# create_puzzle(puzzle_size)
-print("Puzzle created...Show puzzle for you now...")
+    while(puzzle_size != 8 and puzzle_size != 15 and puzzle_size != 24):
+        puzzle_size = int(input(
+            "What size of puzzle you want to have? Enter 8, 15 or 24 Please: "))
+    print("You choose {}-puzzle, now it is  time to create the puzzle for you...".format(puzzle_size))
+    # create_puzzle(puzzle_size)
+    print("Puzzle created...Show puzzle for you now...")
 
-# if __name__ == "__main__":
-#    main()
+
+if __name__ == "__main__":
+    main()
